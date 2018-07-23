00394d19e9ec627f8dd4d7b84da183d5713671ed
Eliot Cowley <eliot93@live.com>
2018-07-22 22:47:19 -0700

    Tried to get multi-display working

--- VRGame/Assets/Scripts/MultiDisplay.cs
+++ VRGame/Assets/Scripts/MultiDisplay.cs
@@ -1,13 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MultiDisplay : MonoBehaviour
 {
+    [SerializeField]
+    private TextMeshProUGUI textMesh;
+
     // Use this for initialization
     void Start()
     {
-        Debug.Log("Displays connected: " + Display.displays.Length);
+        string displayMessage = "Displays connected: " + Display.displays.Length;
+        Debug.Log(displayMessage);
+        textMesh.SetText(displayMessage);
+
+        if (Display.displays.Length > 1)
+        {
+            Display.displays[1].Activate();
+        }
     }
 
     // Update is called once per frame
